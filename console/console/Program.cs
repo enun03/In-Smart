9386100242d64edc65b7e4b9a55105dde1e70d10
xDarkfalconx <julian.e.oquendo@gmail.com>
2016-02-24 21:47:39 -0800

    simple code

---
+++ console/console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.WriteLine("learning how to use git");
+            //ctrl+f5 to run.
+        }
+    }
+}
