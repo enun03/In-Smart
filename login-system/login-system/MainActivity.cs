7d6efcd7f5574564e55106f4bdd88f0798225dba
xDarkfalconx <julian.e.oquendo@gmail.com>
2016-03-04 11:54:01 -0800

    This is the First App initiative.

---
+++ login-system/login-system/MainActivity.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Threading;
+
+namespace login_system
+{
+    [Activity(Label = "login_system",MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity{
+
+        private ProgressBar mProgressBar;
+        private Button mBtnSignUp;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            mBtnSignUp = FindViewById<Button>(Resource.Id.btnsignup);
+            mProgressBar = FindViewById<ProgressBar>(Resource.Id.progressBar1);
+            mBtnSignUp.Click += (object sender, EventArgs args) =>
+           
+            {
+                
+                //pull up dialog
+                FragmentTransaction transaction = FragmentManager.BeginTransaction();
+                dialog_SignUp signUpDialog = new dialog_SignUp();
+                signUpDialog.Show(transaction, "dialog fragment");
+
+                signUpDialog.mOnSignUpComplete += SignUpDialog_mOnSignUpComplete;
+
+            };
+
+        
+
+
+        }
+
+        void SignUpDialog_mOnSignUpComplete(object sender, OnSignUpEventArgs e)
+        {
+            mProgressBar.Visibility = ViewStates.Visible;
+            Thread thread = new Thread(ActLikeARequest);
+            thread.Start();
+           
+        }
+        private void ActLikeARequest()
+        {
+            Thread.Sleep(3000);
+            RunOnUiThread(() => { mProgressBar.Visibility = ViewStates.Invisible; });
+
+        }
+    }
+}
+
